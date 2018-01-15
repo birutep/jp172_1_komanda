de7cb949abb3bd237bad3c3c7ffbc4e2cbdbc87d
Aurimas Cincikas <cincikas.aurimas.jp17@gmail.com>
2018-01-15 19:49:41 +0200

    initial react app

---
+++ book_store_app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Book Store, created by react. Or not.
+      </div>
+    );
+  }
+}
+export default App;
