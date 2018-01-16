cd8f80bc2b5d4ab7382534fff224414dcfd7c6f9
birute polkiene <birute.polkiene@gmail.com>
2018-01-16 12:17:24 +0200

    Buttons from buy_books and wishlist: 	- light and dark, big and small buttons done.

--- book_store_app/src/App.js
+++ book_store_app/src/App.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import pagrindinesKlases from './App.css';
 
 import Footeris from './FooterPvz/Footeris';
-import Mygtukas from './MygtukoPvz/Mygtukas';
+import MygtukasDark from './Mygtukai/MygtukasDark';
+import MygtukasLight from './Mygtukai/MygtukasLight';
+import MygtukasDarkSmall from './Mygtukai/MygtukasDarkSmall';
+import MygtukasLightSmall from './Mygtukai/MygtukasLightSmall';
 
 class App extends Component {
   render() {
     return (
       <div className={pagrindinesKlases.App}>
         <Footeris />
-          <Mygtukas pavadinimas={'Mygtukas1'} />
-          <Mygtukas pavadinimas={'Mygtukas1'} />
-          <Mygtukas pavadinimas={'SuperMygtukas'} />
+        <MygtukasDark pavadinimas={'Dark mygtukas'} />
+        <MygtukasLight pavadinimas={'Light mygtukas'} />
+        <MygtukasDarkSmall pavadinimas={'Dark mini'} />
+        <MygtukasLightSmall pavadinimas={'Light mini'} />
         <Footeris />
       </div>
     );
