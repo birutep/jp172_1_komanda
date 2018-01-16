0af36d1d9b6ca64ef9836baa1ecd2c886bd9c7af
Aurimas Cincikas <cincikas.aurimas.jp17@gmail.com>
2018-01-16 18:51:34 +0200

    Padarytas elementas DivKnyga1 kataloge atvaizduoti knygai ir jame elementas zavigzdutes kur perduodam skaiciu kaip reitinga bei css klase zvaigzduciu spalvai

--- book_store_app/src/App.js
+++ book_store_app/src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import pagrindinesKlases from './App.css';
 
-import Footeris from './FooterPvz/Footeris';
-import Mygtukas from './MygtukoPvz/Mygtukas';
 import Knyga1 from './DivKnyga1/DivKnyga1';
 
 class App extends Component {
   render() {
     return (
       <div className={pagrindinesKlases.App}>
-        <Knyga1 img="IMG/books_book_01.jpg" title="20 budu issivirti koldunams" author="Aurimas cincikas" rating="5"/>
+
       </div>
     );
   }
--- book_store_app/src/DivKnyga1/DivKnyga1.js
+++ book_store_app/src/DivKnyga1/DivKnyga1.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import cssKlases from './DivKnyga1.css';
+import Zvaigzdutes from './Zvaigzdutes/Zvaigzdutes';
 
 
 const knyga1 = ( props ) => {
     return (
-        <div className={cssKlases.books_box}>
+        <div className={cssKlases.oranzine}>
            <img src={props.img} alt="book" />
            <h3>{props.title}</h3>
            <p>by {props.author}</p>
-           <span className={"books_rating_static books_rating_" + (+props.rating*10)}></span>
+           <Zvaigzdutes rating={props.rating} spalva={cssKlases.oranzine}/>
         </div>
     )
 };
 
 export default knyga1;
+
+
+//norint paze App.js faile imetam virsuje
+//import Knyga1 from './DivKnyga1/DivKnyga1';
+
+//o returne 
+
+//<Knyga1 img="IMG/books_book_01.jpg" title="20 budu issivirti koldunams" author="Aurimas Cincikas" rating="5"/>
+//<Knyga1 img="IMG/books_book_02.jpg" title="Pratimai rankai" author="Jurgis Sidlauksas" rating="2.5"/>
