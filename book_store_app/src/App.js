299f3e86c6de83c66211cc6818c0760c8eff0594
Jurgis Šidlauskas <sidlauskas.jurgis@gmail.com>
2018-01-17 13:56:34 +0200

    created component for a single book DivKnyga4 (will rename later)

--- book_store_app/src/App.js
+++ book_store_app/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import pagrindinesKlases from './App.css';
+import DivKnyga4 from './DivaiKnygomsAtvaizduot/DivKnyga4/DivKnyga4';
 
 
 class App extends Component {
   render() {
     return (
       <div className={pagrindinesKlases.App}>
+        <DivKnyga4 img="./IMG/books_book_01.jpg" name="Aurimo patarimai riešų stiprinimui" author="Aurimas Činčikas" price="50" description="Aurimas atskleidžia paslaptį, kaip jam per dvi savaites pavyko sustiprinti dešinės rankos riešą."/>
       </div>
     );
   }
