40af8bf7fe019b48a49dc2d605be584f7143220d
Daniel Rozov <danielrozov@gmail.com>
2018-01-17 10:35:32 +0200

    Added new files: Search.css and Search.js. Modified: App.js, Knopf.js, SubHeader.css, SubHeader.js. Missing line in SubHeader

--- book_store_app/src/App.js
+++ book_store_app/src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
 import pagrindinesKlases from './App.css';
 
-<<<<<<< HEAD
 import Footeris from './FooterPvz/Footeris';
 import Mygtukas from './MygtukoPvz/Mygtukas';
-import Header from './SubHeader/SubHeader';
+import SubHeader from './SubHeader/SubHeader';
 import Knopf from './Knopf/Knopf';
+import Search from './Search/Search'
 
-=======
->>>>>>> f2b36be5704903ab21695b3678a7e45f13735ef4
 class App extends Component {
   render() {
     return (
       <div className={pagrindinesKlases.App}>
-<<<<<<< HEAD
-        <Header></Header>
-        <Knopf parsas="All books"></Knopf>
-        <Knopf parsas="Free books"></Knopf>
-
-=======
-      
->>>>>>> f2b36be5704903ab21695b3678a7e45f13735ef4
+        <SubHeader></SubHeader>
+        <Knopf spausti="All books"></Knopf>
+        <Knopf spausti="Most Recent"></Knopf>
+        <Knopf spausti="Most Popular"></Knopf>
+        <Knopf spausti="Free Books"></Knopf>
+        <Search></Search>
       </div>
     );
   }
--- book_store_app/src/SubHeader/SubHeader.js
+++ book_store_app/src/SubHeader/SubHeader.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import headerioKlase from './SubHeader.css';
 
-const sub = ( props ) => { //sicia props nepanaudota, bet reliaia cia masyvas su viskuo ka perduodame ka rasom pvz <Footeris vardas="kazkoks" kaina="15">, tuomet sicia galiam paimt per props.vardas props.kaina
+const sub = ( props ) => { 
     return (
-        <div className={headerioKlase.test}>
-           Labas!
+        <div className={headerioKlase.book_filter}>
+           <p className={headerioKlase.book_filter_p} >Browse Available Books</p>
         </div>
     )
 };
