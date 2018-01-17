a8daa0958c20f81941c0a102deb7522a41917eb6
Aurimas Činčikas <cincikas.aurimas.jp17@gmail.com>
2018-01-17 14:22:05 +0200

    Started login and register forms :)

--- book_store_app/src/App.js
+++ book_store_app/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import pagrindinesKlases from './App.css';
 
-
 class App extends Component {
   render() {
     return (
       <div className={pagrindinesKlases.App}>
+      
       </div>
     );
   }
