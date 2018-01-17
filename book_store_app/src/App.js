ae956e493b1270f597283e7249ea984108c289e4
Daniel Rozov <danielrozov@gmail.com>
2018-01-17 12:54:24 +0200

    Added new folder SubheaderBottom

--- book_store_app/src/App.js
+++ book_store_app/src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import pagrindinesKlases from './App.css';
 
+import Footeris from './FooterPvz/Footeris';
+import Mygtukas from './MygtukoPvz/Mygtukas';
+import SubHeader from './SubHeader/SubHeader';
+import SubHeaderBottom from './SubHeader/SubheaderBottom/SubheaderBottom';
+import SubheaderBottom from './SubHeader/SubheaderBottom/SubheaderBottom';
+// import Knopf from './Knopf/Knopf';
+// import Search from './Search/Search';
+
 class App extends Component {
   render() {
     return (
       <div className={pagrindinesKlases.App}>
+        <SubHeader name="Browse Available Books"></SubHeader>
+        <SubheaderBottom/>
+        {/* <Knopf spausti="All books"></Knopf>
+        <Knopf spausti="Most Recent"></Knopf>
+        <Knopf spausti="Most Popular"></Knopf>
+        <Knopf spausti="Free Books"></Knopf>
+        <Search></Search> */}
       </div>
     );
   }
---
+++ book_store_app/src/SubHeader/SubheaderBottom/SubheaderBottom.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Knopf from '../../Knopf/Knopf';
+import Search from '../../Search/Search';
+
+const subBottom = ( props ) => { 
+    return (
+        <div>
+           <Knopf spausti="All books"></Knopf>
+           <Knopf spausti="Most Recent"></Knopf>
+           <Knopf spausti="Most Popular"></Knopf>
+           <Knopf spausti="Free Books"></Knopf>
+           <Search></Search>
+        </div>
+    )
+};
+
+export default subBottom;
