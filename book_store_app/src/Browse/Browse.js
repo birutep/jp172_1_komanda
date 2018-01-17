6f635bec95a2b2a2e7f2b0ad4f7d8ce37656d8a6
Aurimas Cincikas <cincikas.aurimas.jp17@gmail.com>
2018-01-17 21:19:11 +0200

    added a bit books data to JSON file, started writing functionality

--- book_store_app/src/Browse/Browse.js
+++ book_store_app/src/Browse/Browse.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import SubHeader from '../SubHeader/SubHeader';
 
+import knygos from '../data/books.json';
+import Knyga from '../DivaiKnygomsAtvaizduot/DivKnyga1/DivKnyga1';
+
 const Browse = ( props ) => {
     return (
         <div>
             <SubHeader />
-            <p>Browse</p>
+            <Knyga img={knygos[0].img} title={knygos[0].title} author={knygos[0].author} rating={knygos[0].rating}/>
+            <Knyga img={knygos[1].img} title={knygos[1].title} author={knygos[1].author} rating={knygos[1].rating}/>
+            <Knyga img={knygos[2].img} title={knygos[2].title} author={knygos[2].author} rating={knygos[2].rating}/>
         </div>
     )
 };
