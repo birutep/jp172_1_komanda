f870441c95c63e35c25fef26fa0cd288469151fc
Aurimas Cincikas <cincikas.aurimas.jp17@gmail.com>
2018-01-17 22:43:24 +0200

    added functions to show all recent free and popular books

--- book_store_app/src/Browse/Browse.js
+++ book_store_app/src/Browse/Browse.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import SubHeader from '../SubHeader/SubHeader';
 
-import knygos from '../data/books.json';
-import Knyga from '../DivaiKnygomsAtvaizduot/DivKnyga1/DivKnyga1';
+import books from '../data/books.json';
+import Book from '../DivaiKnygomsAtvaizduot/DivKnyga1/DivKnyga1';
+
+
+const createBook = (x) => <Book img={x.img} title={x.title} author={x.author} rating={x.rating}/>;
+const free =  books.filter(el => el.price===0); 
+const popular = books.filter(el => el.rating===5);
+const lastest = books.slice(books.length-5, books.length);
+
+
+const allBooks = books.map(createBook);
+const freeBooks = free.map(createBook);
+const popularBooks = popular.map(createBook);
+const newestBooks = lastest.map(createBook);
+
 
 const Browse = ( props ) => {
     return (
         <div>
             <SubHeader />
-            <Knyga img={knygos[0].img} title={knygos[0].title} author={knygos[0].author} rating={knygos[0].rating}/>
-            <Knyga img={knygos[1].img} title={knygos[1].title} author={knygos[1].author} rating={knygos[1].rating}/>
-            <Knyga img={knygos[2].img} title={knygos[2].title} author={knygos[2].author} rating={knygos[2].rating}/>
+            {newestBooks}
         </div>
     )
 };
