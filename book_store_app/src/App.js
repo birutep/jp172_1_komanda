8c66262d97350343b7efc4f81a743e2991e6b844
Laima <laima.valuze@gmail.com>
2018-01-18 10:56:46 +0200

    Corrected styles in Header and Footer

--- book_store_app/src/App.js
+++ book_store_app/src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
         <Header />
         <Nav />
         <Content />
-        <Footer /> */}
+        <Footer />
         {/* <Register /> */}
         {/* <Login />*/}
 
