898b5d6d6870c0d11c43573cc91cbea4eafa2b37
Aurimas Cincikas <cincikas.aurimas.jp17@gmail.com>
2018-01-23 21:30:44 +0200

    Added elements to last 3 pages, need rework css styles to fit design

--- book_store_app/src/BuyBooks/BuyBooks.js
+++ book_store_app/src/BuyBooks/BuyBooks.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import SubHeader from '../SubHeader/SubHeader';
+import SubBot from '../SubHeader/SubHeaderBottomBuyBooks/SubHeaderBottomBuyBooks';
+
+import bookChart from '../data/books.json';                 //kolkas imam ta pati jasona kaip ir visu knygu, imituodami kad krepsel;yje yra 10 knygu, atvaizdavimui ir logikai tikrinti
+import Book from '../DivaiKnygomsAtvaizduot/DivKnyga2/DivKnyga2';
+
+import Gridas from '../Grids/GridBuyBooks/GridBuyBooks';
+
+const createBook = (x) => <Book img={x.img} author={x.author} name={x.krepselioTurinys} description={x.about} price={x.price} /> 
+const krepselioTurinys = bookChart.map( x => createBook(x));
 
 const BuyBooks = ( props ) => {
     return (
         <div>
-            <SubHeader />
-            <p>Buy books</p>
+            <SubHeader name="Buy Books" />
+            <SubBot />  
+            <Gridas elementai={krepselioTurinys} />
         </div>
     )
 };
