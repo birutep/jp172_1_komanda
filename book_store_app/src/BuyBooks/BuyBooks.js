2307781ebab62efd4c0bfb9aeaad2f7d3367473b
Aurimas Činčikas <cincikas.aurimas.jp17@gmail.com>
2018-01-24 12:42:46 +0200

    added functions to buttons in buy books divs

--- book_store_app/src/BuyBooks/BuyBooks.js
+++ book_store_app/src/BuyBooks/BuyBooks.js
@@ -9,7 +9,18 @@ import Book from '../DivaiKnygomsAtvaizduot/DivKnyga2/DivKnyga2';
 
 import Gridas from '../Grids/GridBuyBooks/GridBuyBooks';
 
-const createBook = (x) => <Book img={x.img} author={x.author} name={x.krepselioTurinys} description={x.about} price={x.price} /> 
+//trys funkcijos kabinamos and divKnyga21 mygtuklu pridet, atimty ir pasalint, elementai skirtingi nezinau kaip jiem susibendraut, kad zinot kuris mygtukas iskviete, kad butentt a knyga prideti ar pasalint
+const remove = () =>{
+    console.log("as tureciau pasalinti sita knyga is buy listo, tik nezianu kas mane iskviete ir ka salint");
+}
+const increase = () =>{
+    console.log("as tureciau prideti viena knyga prie tos rusies knygu kiekio krepselyje, bet nezinau kuris mygtukas mane iskviete ir kam pridet");
+}
+const decrease = () =>{
+    console.log("as tureciau patikrint ar kiekis teigiamas jei taip atimti viena knyga prie tos rusies knygu kiekio krepselyje, bet nezinau kuris mygtukas mane iskviete ir is ko atimt");
+}
+
+const createBook = (x) => <Book key={x.id} img={x.img} author={x.author} name={x.krepselioTurinys} description={x.about} price={x.price} remove={remove} increase={increase} decrease={decrease} /> 
 //nurodai knygos indeksa, is visu turimu knygu randa ir grazina knyga, nes krepselyje saugosim tik indeksus ir kieki
 const getBookById = (bookid) =>{        
      const book = allbooks.filter((item) => item.id==bookid);
