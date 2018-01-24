2fc1354ec5e376c7dc66165b8691aef770be7e8a
birute polkiene <birute.polkiene@gmail.com>
2018-01-24 10:00:26 +0200

    CSS:  - Tarpas tarp booterio ir grid - patvarkytas

--- book_store_app/src/BuyBooks/BuyBooks.js
+++ book_store_app/src/BuyBooks/BuyBooks.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SubHeader from '../SubHeader/SubHeader';
 import SubBot from '../SubHeader/SubHeaderBottomBuyBooks/SubHeaderBottomBuyBooks';
 
-import bookChart from '../data/books.json';                 //kolkas imam ta pati jasona kaip ir visu knygu, imituodami kad krepsel;yje yra 10 knygu, atvaizdavimui ir logikai tikrinti
+import bookChart from '../data/books_buybooks_forTesting_bp.json';                 //kolkas imam ta pati jasona kaip ir visu knygu, imituodami kad krepsel;yje yra 10 knygu, atvaizdavimui ir logikai tikrinti
 import Book from '../DivaiKnygomsAtvaizduot/DivKnyga2/DivKnyga2';
 
 import Gridas from '../Grids/GridBuyBooks/GridBuyBooks';
