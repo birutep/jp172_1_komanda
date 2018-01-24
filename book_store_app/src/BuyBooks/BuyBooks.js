43150f8b588a9cb0922c1f0b1a0adf7bf8e940ab
Aurimas Činčikas <cincikas.aurimas.jp17@gmail.com>
2018-01-24 11:24:04 +0200

    started functionality of buy books page

--- book_store_app/src/BuyBooks/BuyBooks.js
+++ book_store_app/src/BuyBooks/BuyBooks.js
@@ -2,16 +2,29 @@ import React from 'react';
 import SubHeader from '../SubHeader/SubHeader';
 import SubBot from '../SubHeader/SubHeaderBottomBuyBooks/SubHeaderBottomBuyBooks';
 
-import bookChart from '../data/books.json';                 //kolkas imam ta pati jasona kaip ir visu knygu, imituodami kad krepsel;yje yra 10 knygu, atvaizdavimui ir logikai tikrinti
+import bookChart from '../data/chart.json';             //kintamasis krepseliui, laikysim tik id ir kieki
+import allbooks from '../data/books.json';  
+
 import Book from '../DivaiKnygomsAtvaizduot/DivKnyga2/DivKnyga2';
 
 import Gridas from '../Grids/GridBuyBooks/GridBuyBooks';
 
 const createBook = (x) => <Book img={x.img} author={x.author} name={x.krepselioTurinys} description={x.about} price={x.price} /> 
-
-const notFreeBooks =bookChart.filter(item => item.price >0);        //jei nemokama knyga kam jha pirkti?
-const krepselioTurinys = notFreeBooks.map( x => createBook(x));     //sukuriam divukus
-const getTotalPrice = () => notFreeBooks.reduce(function (acc, obj) { return acc + obj.price; }, 0);    //paskaiciuojam krepselio visu knygu kaina
+//nurodai knygos indeksa, is visu turimu knygu randa ir grazina knyga, nes krepselyje saugosim tik indeksus ir kieki
+const getBookById = (bookid) =>{        
+     const book = allbooks.filter((item) => item.id==bookid);
+     return book[0];
+}
+//bega per krepseli ima id ir pagal id vis prideda knyga, krepselio turinys tampa divu su knygom krepselyje rinkiniu
+const krepselioTurinys = bookChart.map( x => createBook(getBookById(x.id)));   
+const getTotalPrice = () => {
+    let sum =0;
+    for(let i= 0; i<bookChart.length; i++){
+        let book = getBookById(bookChart[i].id);
+        sum+= (book.price * bookChart[i].count);
+    }
+    return sum;
+}
 const BuyBooks = ( props ) => {
     return (
         <div>
